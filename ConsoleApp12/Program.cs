6026a84688130e5b87e591e781e6b38fcd293e62
alimovabdulla <abdullaalimov555@gmail.com>
2024-08-06 22:08:29 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,65 @@
+﻿
+/*ATM maşınları 4 və ya 6 rəqəmli PİN kodlara icazə verir və PİN kodlar tam 4 və ya 6 rəqəmdən başqaheç nə
+ ehtiva edə bilməz. Funksiya etibarlı PIN sətrini ötürübsə, qaytarın true, əks halda qaytarın false*/
+#region 
+//public class Kata
+//{
+//    public bool ValidatePin(string pin)
+//    {
+//        if (pin.Length != 6 && pin.Length != 4) return false;
+//        return true;
+
+
+
+//    }
+//}
+
+#endregion
+
+/*Sətirdə eyni miqdarda "x" və "o" hərflərinin olub olmadığını yoxlayın.
+Metod boolean qaytarmalı və hərflərə həssas olmalıdır. Sətirdə istənilən simvol ola bilər.*/
+#region
+
+//public class Kata
+//{
+//    public bool XO(string input)
+//    {
+//        int Xcount = 0;
+//        int Ocount = 0;
+
+//        foreach (char item in input)
+//        {
+//            if (item == 'x' || item == 'X')
+//            {
+//                Xcount++;
+
+//            }
+//            else if (item == 'o' || item == 'O')
+//            {
+
+//                Ocount++;
+//            }
+//        }
+//        return Xcount == Ocount;
+
+//    }
+//}
+#endregion
+
+/*Sadə, bir sıra sözlər verildikdə, ən qısa söz(lər)in uzunluğunu qaytarın.*/
+#region
+//public static class Kata
+//{
+//    public static int FindShort(string s)
+//    {
+//        if (s == null) { return 0; }
+//        string[] sozler = s.Split(' ');
+//        int minsoz = sozler.Min(x => x.Length);
+//        return minsoz;
+//    }
+//}
+
+#endregion
+
+
+ 
